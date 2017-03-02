5885355a40b4c6b3af7171d9a7be150a2cd59cbb
alinko <alinkokomansuby@gmail.com>
2017-03-02 14:49:46 +0700

    Update avb.php

--- avb.php
+++ avb.php
@@ -10,51 +10,65 @@
 ---------------------------------------------------------------
 **/
 
-# warna
-$a_m="\033[1;31m"; # merah
-$a_k="\033[1;33m"; # kuning
-$a_h="\033[1;32m"; # hijau
-$a_n="\033[0m";    # netral
-$a_b="\033[1;34m"; # biru
 
-function a_xXx($url){
-@define(ch,curl_init());
-curl_setopt(ch,CURLOPT_URL,$url);
-curl_setopt(ch,CURLOPT_RETURNTRANSFER,1);
-curl_setopt(ch,CURLOPT_USERAGENT,"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
-curl_setopt(ch,CURLOPT_COOKIEJAR,"a_cookie-".time().".txt");
-curl_setopt(ch,CURLOPT_COOKIEFILE,"a_cookie-".time().".txt");
-curl_setopt(ch,CURLOPT_FOLLOWLOCATION,1);
-curl_setopt(ch,CURLOPT_BINARYTRANSFER,1);
-$r=curl_exec(ch);
-$h=curl_getinfo(ch,CURLINFO_HTTP_CODE);
-$valid['httpcode'] = $h;
-    if ($h == 200) {
-        $valid['result'] = true;
-    } else {
-        $valid['result'] = false;
-    }
-    return $valid;
-curl_close(ch);
+
+class Avb{
+	public $UserAgent;
+	public $Alamat;
+	public $Url;
+	public $random;
+	public $pisahUrl;
+	public function visit($url,$ua){
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch,CURLOPT_USERAGENT,$ua);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_HEADER,0);
+		curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+		$e=curl_exec($ch);
+		curl_close($ch);
+		return $e;
+	}
+	public function pisahUrl($list){
+		$fgt = file_get_contents($list);
+		$exp = explode("\n",$fgt);
+		return $exp;
+	}
+	public function bannerX(){
+		@system('clear');
+echo "    ___     ______          ____    \n";
+echo "   / \ \   / / __ )  __   _|___ \   \n";
+echo "  / _ \ \ / /|  _ \  \ \ / / __) |  \n";
+echo " / ___ \ V / | |_) |  \ V / / __/   \n";
+echo "/_/   \_\_/  |____/    \_/ |_____|  \n";
+echo "                                    \n";
+echo "+-----------------------------------+\n";
+echo "|   Auto Visitor Blog Version 2.0   |\n";
+echo "| Codename        : MakanBang!      |\n";
+echo "|  Version        : 2.0             |\n";
+echo "|   Author        : shutdown57      |\n";
+echo "+-----------------------------------+\n";
+
+	}
 }
-function a_banner(){
-@system('clear');
-echo "+------------------------------------+\n";
-echo "|".$GLOBALS['a_m']."  ~ [ AutoVisitor Blog v1 CLI] ~ ".$GLOBALS['a_n']."   |\n";
-echo "|------------------------------------|\n";
-echo "| ".$GLOBALS['a_k']."c0ded by : alinko a.k.a shutdown57".$GLOBALS['a_n']." |\n";
-echo "|------------------------------------|\n";
-echo "| ".$GLOBALS['a_b']."linuXcode.org -".$GLOBALS['a_m']." dracos-linux.org ".$GLOBALS['a_n']."  |\n";
-echo "+------------------------------------+\n";
-}
-a_banner();
-echo $a_b."MASUKAN URL :".$a_n; $a_url=trim(fgets(STDIN));
-echo $a_k."BERAPA?     :".$a_n; $a_brp=trim(fgets(STDIN));
-	$u=(!preg_match("/^http:|^https:/",$a_url)) ? "http://".$a_url : $u=$a_url;
-	for($i=1;$i<=$a_brp;$i++){
-	if(a_xXx($u)){
-		echo "[".$a_h."DONE".$a_n."][".$a_b."$i".$a_n."] ".$u."\n";
-	}else{
-		echo "[".$a_m."FAIL".$a_n."][".$a_b."$i".$a_n."] ".$u."\n";
+
+$avb = new Avb();
+$avb->UserAgent=array(
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+	"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0");
+$avb->random=rand(0,1);
+$avb->bannerX();
+
+echo "list url blog  : "; $l = trim(fgets(STDIN));
+echo "berapa banyak  : "; $b = trim(fgets(STDIN));
+$avb->Alamat=$avb->pisahUrl($l);
+$i=1;
+foreach($avb->Alamat as $avb->Url){
+		for($x=1;$x<=$b;$x++){
+	if($avb->visit($avb->Url,$avb->UserAgent[$avb->random])){
+		@system('clear');
+		echo "[ ".$x." ] ".$avb->Url."\n";
+		echo "Waiting For You ...";
+	}
 	}
 }
