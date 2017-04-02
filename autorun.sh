725b9f21ef70dca66a43f7375e3bf6d64bcb9bcb
shutdown57 a.k.a alinko-kun <alinkokomansuby@gmail.com>
2017-04-02 13:44:17 +0700

    Update

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-php /opt/avb/avb.php
+#---------------------------------------------------------------
+# c0ded by  : alinko a.k.a shutdown57
+# contact   : alinkokomansuby@gmail.com
+# Thanks to :
+# indoXploit.or.id - stackoverflow.com - php.net - curl.haxx.se
+#---------------------------------------------------------------
+# copyright (c) 2k17 linuXcode.org
+#-------------------------------------------------------------
+if [[ `whoami` == "root" ]]; then
+	php /opt/avb/avb.php
+else
+	echo "[+] Kamu belum menjadi Mode Sage. :v"
+	echo "[+] sage maksutnya root "
+fi
