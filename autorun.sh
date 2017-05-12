d9bd26325aa5f9d4313bb7915128e118ff53fd9c
shutdown57 a.k.a alinko-kun <alinkokomansuby@gmail.com>
2017-05-12 11:47:13 +0700

    Add files via upload

--- autorun.sh
+++ autorun.sh
@@ -8,8 +8,23 @@
 #---------------------------------------------------------------
 # copyright (c) 2k17 linuXcode.org
 #-------------------------------------------------------------
+
+#/* AutoVisitor Blog v2.8
+#\* c0ded by : shutdown57 a.k.a alinko
+#/*----------------------------------
+#\*| Hargai karya orang kawand. :)  
+#/*| Jangan kaya BOCAH ganti nama author segala.
+#\*| Kalo cuma bisa "pake" ya, pake saja.. gak usah sok tau,
+#/*| Ganti Nama author segala, Emang situ dah Pro?
+#\*| Kalo dah "PRO" Bisa semua nya,buat sendiri lah bego.
+#/*| Nyadar kalo cuma bisa "pake" , hargai gan!
+#\*| NB : Setidaknya kalo recode tulis "recode by: " dan "Original code by:  "
+#/*|    - Biar apa? biar gak bego kayak bocah.
+#\*----------------------------------
+#*/
+
 if [[ `whoami` == "root" ]]; then
-	php /opt/avb/avb.php
+	php /opt/avb/__avb__.php
 else
 	echo "[+] Kamu belum menjadi Mode Sage. :v"
 	echo "[+] sage maksutnya root "
